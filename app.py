0bd4b6ed9bad881805e4ed1e40a030fcd739d3a2
Vitorfran <vitorpoizon@gmail.com>
2025-04-25 22:21:12 -0300

    Adaptando para o render

--- app.py
+++ app.py
@@ -180,4 +180,5 @@ def excluir_pedido(pedido_id):
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
